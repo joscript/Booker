cd675ae9bc850006f72331baa96e4afb41498df9
joscript <josua.lagat@gmail.com>
2020-07-03 16:46:23 +0800

    added transistion animation to basket item deletion

--- public/js/app.js
+++ public/js/app.js
@@ -2020,6 +2020,8 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 //
 //
 //
+//
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   computed: _objectSpread(_objectSpread({}, Object(vuex__WEBPACK_IMPORTED_MODULE_0__["mapGetters"])(["itemsInBasket"])), Object(vuex__WEBPACK_IMPORTED_MODULE_0__["mapState"])({
@@ -61475,77 +61477,85 @@ var render = function() {
             ])
           ]),
           _vm._v(" "),
-          _vm._l(_vm.basket, function(item) {
-            return _c("div", { key: item.bookable.id }, [
-              _c(
-                "div",
-                {
-                  staticClass: "py-2 border-top d-flex justify-content-between"
-                },
-                [
-                  _c(
-                    "span",
-                    [
-                      _c(
-                        "router-link",
-                        {
-                          staticClass: "text-dark",
-                          attrs: {
-                            to: {
-                              name: "bookable",
-                              params: { id: item.bookable.id }
-                            }
-                          }
-                        },
-                        [_vm._v(_vm._s(item.bookable.title))]
-                      )
-                    ],
-                    1
-                  ),
-                  _vm._v(" "),
-                  _c("span", { staticClass: "font-weight-bold" }, [
-                    _vm._v("$" + _vm._s(item.price.total))
-                  ])
-                ]
-              ),
-              _vm._v(" "),
-              _c(
-                "div",
-                { staticClass: "py-1 d-flex justify-content-between" },
-                [
-                  _c("span", [
-                    _c("small", [
-                      _vm._v(" From " + _vm._s(item.dates.from) + " ")
-                    ])
-                  ]),
-                  _vm._v(" "),
-                  _c("span", [
-                    _c("small", [_vm._v(" To " + _vm._s(item.dates.to) + " ")])
-                  ])
-                ]
-              ),
-              _vm._v(" "),
-              _c("div", { staticClass: "py-1 text-right" }, [
+          _c(
+            "transition-group",
+            { attrs: { name: "fade" } },
+            _vm._l(_vm.basket, function(item) {
+              return _c("div", { key: item.bookable.id }, [
                 _c(
-                  "button",
+                  "div",
                   {
-                    staticClass: "btn btn-sm btn-outline-secondary",
-                    on: {
-                      click: function($event) {
-                        return _vm.$store.dispatch(
-                          "removeFromBasket",
-                          item.bookable.id
-                        )
-                      }
-                    }
+                    staticClass:
+                      "py-2 border-top d-flex justify-content-between"
                   },
-                  [_c("i", { staticClass: "fas fa-trash-alt" })]
-                )
+                  [
+                    _c(
+                      "span",
+                      [
+                        _c(
+                          "router-link",
+                          {
+                            staticClass: "text-dark",
+                            attrs: {
+                              to: {
+                                name: "bookable",
+                                params: { id: item.bookable.id }
+                              }
+                            }
+                          },
+                          [_vm._v(_vm._s(item.bookable.title))]
+                        )
+                      ],
+                      1
+                    ),
+                    _vm._v(" "),
+                    _c("span", { staticClass: "font-weight-bold" }, [
+                      _vm._v("$" + _vm._s(item.price.total))
+                    ])
+                  ]
+                ),
+                _vm._v(" "),
+                _c(
+                  "div",
+                  { staticClass: "py-1 d-flex justify-content-between" },
+                  [
+                    _c("span", [
+                      _c("small", [
+                        _vm._v(" From " + _vm._s(item.dates.from) + " ")
+                      ])
+                    ]),
+                    _vm._v(" "),
+                    _c("span", [
+                      _c("small", [
+                        _vm._v(" To " + _vm._s(item.dates.to) + " ")
+                      ])
+                    ])
+                  ]
+                ),
+                _vm._v(" "),
+                _c("div", { staticClass: "py-1 text-right" }, [
+                  _c(
+                    "button",
+                    {
+                      staticClass: "btn btn-sm btn-outline-secondary",
+                      on: {
+                        click: function($event) {
+                          return _vm.$store.dispatch(
+                            "removeFromBasket",
+                            item.bookable.id
+                          )
+                        }
+                      }
+                    },
+                    [_c("i", { staticClass: "fas fa-trash-alt" })]
+                  )
+                ])
               ])
-            ])
-          })
+            }),
+            0
+          )
         ],
-        2
+        1
       )
     ])
   ])
--- resources/js/basket/Basket.vue
+++ resources/js/basket/Basket.vue
@@ -13,27 +13,29 @@
                     </h6>
                 </div>
 
-                <div v-for="item in basket" :key="item.bookable.id">
-                    <div class="py-2 border-top d-flex justify-content-between">
-                        <span>
-                            <router-link class="text-dark" :to="{name: 'bookable', params: {id: item.bookable.id}}">{{item.bookable.title}}</router-link>
-                        </span>
-                        <span class="font-weight-bold">${{item.price.total}}</span>
+                <transition-group name="fade">
+                    <div v-for="item in basket" :key="item.bookable.id">
+                        <div class="py-2 border-top d-flex justify-content-between">
+                            <span>
+                                <router-link class="text-dark" :to="{name: 'bookable', params: {id: item.bookable.id}}">{{item.bookable.title}}</router-link>
+                            </span>
+                            <span class="font-weight-bold">${{item.price.total}}</span>
+                        </div>
+                        <div class="py-1 d-flex justify-content-between">
+                            <span>
+                                <small> From {{item.dates.from}} </small>
+                            </span>
+                            <span>
+                                <small> To {{item.dates.to}} </small>
+                            </span>
+                        </div>
+                        <div class="py-1 text-right">
+                            <button class="btn btn-sm btn-outline-secondary" @click="$store.dispatch('removeFromBasket', item.bookable.id)">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                        </div>
                     </div>
-                    <div class="py-1 d-flex justify-content-between">
-                        <span>
-                            <small> From {{item.dates.from}} </small>
-                        </span>
-                        <span>
-                            <small> To {{item.dates.to}} </small>
-                        </span>
-                    </div>
-                    <div class="py-1 text-right">
-                        <button class="btn btn-sm btn-outline-secondary" @click="$store.dispatch('removeFromBasket', item.bookable.id)">
-                            <i class="fas fa-trash-alt"></i>
-                        </button>
-                    </div>
-                </div>
+                </transition-group>
 
 
             </div>
